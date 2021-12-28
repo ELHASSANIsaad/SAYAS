bc9227ac9ada245c85728309bbbcd84ff63c2f17
lulumayuri <84724511+lulumayuri@users.noreply.github.com>
2021-12-28 13:06:10 +0100

    youa

--- sayas-front/src/App.js
+++ sayas-front/src/App.js
@@ -1,12 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import Login from "./pages/login";
+import Dashboard from "./pages/dashboard";
+import Index from "./pages/index";
+import AddPage from "./pages/add";
+import EditPage from "./pages/edit";
+import Register from "./pages/register";
+import NotFound from "./pages/notfound";
+import FileUploadPage from "./pages/fileupload";
 
-function App() {
-  return (
-    <div className="App">
-      <h2> Hello SAYAS, front-end</h2>
-    </div>
-  );
+class App extends Component {
+
+    render() {
+        return (
+            <div className="App" style={{ 
+                backgroundImage: `url("https://wallpaperaccess.com/full/952192.jpg")`,
+                backgroundPosition: 'center',
+                backgroundSize: 'cover',
+                backgroundRepeat: 'no-repeat',
+                width: '100vw',
+                height: '100vh'
+              }}>
+                <Router>
+                    <Switch>
+                        <Route exact path='/' component={Login} />
+                        <Route path='/dashboard' component={Dashboard} />
+                        <Route path='/index' component={Index}/>
+                        <Route path='/register' component={Register} />
+                        <Route path='/add' component={AddPage} />
+                        <Route path='/edit/' component={EditPage} />
+                        <Route path='/fileupload/' component={FileUploadPage} />
+                        <Route path='*' component={NotFound} />
+                    </Switch>
+                </Router>
+            </div>
+        );
+    }
 }
 
 export default App;
